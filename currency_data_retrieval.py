ac51dc525f5bdf77cdd00789ea4f01301d7879f2
Peter Stangolis <pstangolis@gmail.com>
2022-06-02 12:50:59 -0400

    add st.experimental_memo to improve speed of app - caching

--- currency_data_retrieval.py
+++ currency_data_retrieval.py
@@ -5,9 +5,12 @@
 from datetime import datetime, timedelta
 import datetime
 
+import streamlit as st
+
 from yahoofinancials import YahooFinancials
 
 ## EURCAD=X
+@st.experimental_memo
 def eurcad_x(c):
     eur_can = YahooFinancials(c)
     eur_to_can = round(eur_can.get_current_price(),3)
@@ -15,6 +18,7 @@ def eurcad_x(c):
     return eur_can, eur_to_can, prev_close_eur_can
 
 ## CADEUR=X
+@st.experimental_memo
 def cadeur_x(c):
     can_eur = YahooFinancials(c)
     can_to_eur = round(can_eur.get_current_price(),3)
@@ -22,6 +26,7 @@ def cadeur_x(c):
     return can_eur, can_to_eur, prev_close_can_eur
 
 ## USDCAD=X
+@st.experimental_memo
 def usdcad_x(c):
     usd_can = YahooFinancials('USDCAD=X')
     usd_to_can = round(usd_can.get_current_price(),3)
@@ -29,6 +34,7 @@ def usdcad_x(c):
     return usd_can, usd_to_can, prev_close_us_can
 
 ## CADUSD=X
+@st.experimental_memo
 def cadusd_x(c):
     can_us = YahooFinancials('CADUSD=X')
     can_to_us = can_us.get_current_price()
@@ -36,6 +42,7 @@ def cadusd_x(c):
     return can_us, can_to_us, prev_close_can_us
 
 ##GBPCAD=X
+@st.experimental_memo
 def gbpcad_x(c):
     gbp_can = YahooFinancials('GBPCAD=X')
     gbp_to_can = round(gbp_can.get_current_price(),3)
@@ -43,6 +50,7 @@ def gbpcad_x(c):
     return gbp_can, gbp_to_can, prev_close_gbp_can
 
 ##CADGBP=X
+@st.experimental_memo
 def cadgbp_x(c):
     can_gbp = YahooFinancials('CADGBP=X')
     can_to_gbp = round(can_gbp.get_current_price(),2)
